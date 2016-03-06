6acab5e403d6bb0b74ff0d2789104511796db445
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-03-06 10:07:26 +0100

    Redundant controller removed.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,12 +3,3 @@ var abApp = angular.module('address-book', ['LocalStorageModule']);
 abApp.config(['localStorageServiceProvider', function (localStorageServiceProvider) {
 	localStorageServiceProvider.setPrefix('address-book');
 }]);
-
-
-abApp.controller('MainController', ['$scope', function($scope) {
-
-	//var countries = CountryListFactory.getCountryList();
-
-	console.log(countries);
-
-}]);
