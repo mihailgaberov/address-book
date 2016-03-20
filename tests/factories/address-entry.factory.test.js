f5a104867ce69c7c47532f051bc94ab1273b34a2
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-03-20 17:25:12 +0100

    Added unit tests for address-entry factory

--- tests/factories/address-entry.factory.test.js
+++ tests/factories/address-entry.factory.test.js
@@ -2,11 +2,22 @@ describe('AddressEntryFactory:', function () {
 
 	var $factory;
 
+	var mockedRecord = {
+		'id': '1',
+		'firstName': 'Mihail',
+		'lastName': 'Gaberov',
+		'email': 'me@mihail-gaberov.eu',
+		'country': 'Bulgaria'
+	};
+
 	beforeEach(function () {
 		module('address-book-factories');
 		module(function($provide) {
 			$provide.service('localStorageService', function () {
 				return {
+					length: function() {
+						return 1;
+					},
 					set: function () {
 						console.log('>>> set local storage');
 					},
@@ -30,12 +41,27 @@ describe('AddressEntryFactory:', function () {
 
 	beforeEach(inject(function(_AddressEntryFactory_){
 		$factory = _AddressEntryFactory_;
+
+		spyOn($factory, 'addEntry');
+
+		$factory.addEntry(mockedRecord);
 	}));
+
+	it("creates spies for each requested function", function() {
+		expect($factory.getKey).toBeDefined();
+		expect($factory.addEntry).toBeDefined();
+		expect($factory.deleteEntry).toBeDefined();
+		expect($factory.editEntry).toBeDefined();
+		expect($factory.updateEntry).toBeDefined();
+		expect($factory.getAllEntries).toBeDefined();
+	});
 
 	it('getKey() should return a key used in the local storage by given id', function () {
 		var key = $factory.getKey(1);
 		expect(key).toEqual('entry:1');
 	});
 
-
+	it('addEntry() should be called with a record object', function () {
+		expect($factory.addEntry).toHaveBeenCalledWith(mockedRecord);
+	});
 });
